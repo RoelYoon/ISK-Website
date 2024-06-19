24af5b8b975577f4a54b8c5b54f0470d746a0f7a
RoelYoon <roelyoon2@gmail.com>
2024-06-19 23:50:11 +0900

    Update

--- db.js
+++ db.js
@@ -2,16 +2,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'roelyoon',
   password: 'love012',
-  host: '35.203.145.230',
-  port: 5432, // default Postgres port
+  host: 'localhost',
+  port: 0, // default Postgres port
   database: 'ISK'
-});
-pool.connect()
-.then(() => {
-    console.log('Connected to PostgreSQL database');
-})
-.catch((err) => {
-    console.error('Error connecting to PostgreSQL database', err);
 });
 module.exports = {
   query: (text, params) => pool.query(text, params)
