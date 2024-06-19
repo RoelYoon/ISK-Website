4c4840e0186db550d9358408c98ac1df4aeade5a
RoelYoon <roelyoon2@gmail.com>
2024-06-19 21:39:00 +0900

    Updaate

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require('./db');
 const path = require("path");
 const app = express();
-const fs = require('node:fs');
+const fs = require('fs');
 app.use(express.static(path.join(__dirname,"./public")));
 app.use(express.static(path.join(__dirname,"./node_modules")));
 app.use(express.urlencoded({
