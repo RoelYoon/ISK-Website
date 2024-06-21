955deb15695892298282a983f652ec99e2d9f5fe
RoelYoon <roelyoon2@gmail.com>
2024-06-21 18:50:30 +0900

    Update

--- server.js
+++ server.js
@@ -30,15 +30,26 @@ app.get(`/${process.env.KEY}`,async(req,res)=>{
     await res.sendFile(path.resolve(__dirname,"./upload.html"));
 })
 app.post(`/upload`,
- async (req,res)=>{
-    try{
-        await db.query('INSERT INTO article (title, author, date, img, category, content) VALUES ($1,$2,$3,$4,$5,$6)',[req.body.title,req.body.author,req.body.date,req.body.img,req.body.category,req.body.content]);
-        res.redirect(process.env.ADDRESS);
-    }catch (err){
-        console.error(err);
-        res.statusMessage(500).send('Internal Server Error');
+    async (req,res)=>{
+        try{
+            await db.query('INSERT INTO article (title, author, date, img, category, content) VALUES ($1,$2,$3,$4,$5,$6)',[req.body.title,req.body.author,req.body.date,req.body.img,req.body.category,req.body.content]);
+            res.redirect(process.env.ADDRESS);
+        }catch (err){
+            console.error(err);
+            res.statusMessage(500).send('Internal Server Error');
+        }
     }
-}
+)
+app.post(`/delete`,
+    async (req,res)=>{
+        try{
+            await db.query('DELETE FROM article WHERE id=$1',[req.body.id]);
+            res.redirect(process.env.ADDRESS);
+        }catch (err){
+            console.error(err);
+            res.statusMessage(500).send('Internal Server Error');
+        }
+    }
 )
 app.all('*',(req,res)=>{
     res.send("<h1>404 boiii</h1>").status(404);
