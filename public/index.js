258fcc39379ade9ce05de83b61173f6f8746c9e7
RoelYoon <roelyoon2@gmail.com>
2024-06-21 16:25:39 +0900

    Update

--- public/index.js
+++ public/index.js
@@ -16,14 +16,16 @@ function httpGetAsync(theUrl, callback){
     xmlHttp.send(null);
 
 }
-while(document.readyState != 'complete'){}
-
-httpGetAsync("http://35.203.145.230:8099/articles",(res)=>{
-    var js = JSON.parse(res);
-    console.log(js);
-    for(var i = 0; i < js.length; i++){
-        console.log(js[i]);
-        displayArticle(js[i]);
-        document.write("<br>");
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        httpGetAsync("http://35.203.145.230:8099/articles",(res)=>{
+            var js = JSON.parse(res);
+            console.log(js);
+            for(var i = 0; i < js.length; i++){
+                console.log(js[i]);
+                displayArticle(js[i]);
+                document.write("<br>");
+            }
+        });
     }
-});
+  };
