884a4d9eeab155b0f5ce53245a3760a2f020041c
RoelYoon <roelyoon2@gmail.com>
2024-06-21 18:09:48 +0900

    Update

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config()
 const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 0, // default Postgres port
-  database: 'ISK'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  database: process.env.DB
 });
 module.exports = {
   query: (text, params) => pool.query(text, params)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require('./db');
 const path = require("path");
+require('dotenv').config()
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 app.use(express.static(path.join(__dirname,"./node_modules")));
@@ -11,7 +12,6 @@ app.get("/",
 async (req,res)=>{
     await res.sendFile(path.resolve(__dirname,"./public/index.html"));
 })
-var artCount = 0;
 app.get('/articles', async (req, res) => {
     try {
         if(!req.query.id){
@@ -31,7 +31,7 @@ app.post("/upload",
  async (req,res)=>{
     try{
         await db.query('INSERT INTO article (title, author, date, img, category, content) VALUES ($1,$2,$3,$4,$5,$6)',[req.body.title,req.body.author,req.body.date,req.body.img,req.body.category,req.body.content]);
-        res.redirect("http://35.203.145.230:8099/");
+        res.redirect(process.env.ADDRESS);
     }catch (err){
         console.error(err);
         res.statusMessage(500).send('Internal Server Error');
@@ -41,4 +41,4 @@ app.post("/upload",
 app.all('*',(req,res)=>{
     res.send("<h1>404 boiii</h1>").status(404);
 });
-app.listen(8099, "10.138.0.2", ()=>{console.log("Listening at IP 35.203.145.230 port 8099")});
+app.listen(process.env.PORT, process.env.INTERNAL_IP, ()=>{console.log(`Listening at ${process.env.ADDRESS}`)});
