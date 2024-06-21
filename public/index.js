a8175049c78a5d09ba7fd1b0c881a9a6aad78bd4
RoelYoon <roelyoon2@gmail.com>
2024-06-21 17:36:42 +0900

    Update

--- public/index.js
+++ public/index.js
@@ -19,10 +19,23 @@ function httpGetAsync(theUrl, callback){
 
 httpGetAsync("http://35.203.145.230:8099/articles",(res)=>{
     var js = JSON.parse(res);
-    console.log(js[0].date);
-    console.log(parseInt(js[0].date.substring(0,4)));
-    console.log(parseInt(js[0].date.substring(5,7)));
-    console.log(parseInt(js[0].date.substring(8,10)));
+    js.sort(function(a,b){
+        var yearA = parseInt(a.date.substring(0,4));
+        var yearB = parseInt(b.date.substring(0,4));
+        if(yearA==yearB){
+            var monthA = parseInt(a.date.substring(5,7));
+            var monthB = parseInt(b.date.substring(5,7));
+            if(monthA==monthB){
+                var dayA = parseInt(a.date.substring(8,10));
+                var dayB = parseInt(b.date.substring(8,10));
+                return dayA < dayB;
+            }else{
+                return monthA < monthB;
+            }
+        }else{
+            return yearA < yearB;
+        }
+    })
     for(var i = 0; i < js.length; i++){
         displayArticle(js[i]);
         document.body.appendChild(document.createElement("br"));
