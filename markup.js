1eadf7adafbc4f25142e934e2fce016bf169ba5d
RoelYoon <roelyoon2@gmail.com>
2024-06-21 20:20:56 +0900

    Update

--- markup.js
+++ markup.js
@@ -1,7 +1,7 @@
 function convertContent(content){
     var result = "";
     var p = 0;
-    crossOriginIsolated.log(content.substring(p++));
+    console.log(content.substring(p++));
     return result;
 }
 function convert(article){
