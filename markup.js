578c939eb86e5dbf403c2f40a0820fb1c0af6a20
RoelYoon <roelyoon2@gmail.com>
2024-06-21 21:16:12 +0900

    Update

--- markup.js
+++ markup.js
@@ -1,8 +1,60 @@
 function convertContent(content){
     var result = "";
     var p = 0;
+    var stack = [];
+    const keywords = ['#bf{','#it{','#ref{','#GDimg{','#img{','{','}']
+    const idMap = new Map([
+        ['#bf{', 0], 
+        ['#it{', 1],
+        ['#ref{', 2], 
+        ['#GDimg{', 3],
+        ['#img{', 4], 
+        ['{', 5],
+        ['}', 6],
+    ]);
+    const open = new Map([
+        [0,'<strong>'], 
+        [1,'<em>'],
+        [2,'<a href=\''], 
+        [3,''],
+        [4,'<img src=\''], 
+        [5,''],
+        [6,''],
+    ]);
+    const close = new Map([
+        [0,'</strong>'], 
+        [1,'</em>'],
+        [2,'>'], 
+        [3,''],
+        [4,'/>'], 
+        [5,'</a>'],
+        [6,''],
+    ]);
     while(p<content.length){
-    console.log(content.substring(p++));
+        var c = content.substring(p);
+        var minInd = 1000000000;
+        var type = "";
+        for(var i = 0; i < keywords.length; i++){
+            var ind = c.search(keywords[i]);
+            if(ind!=-1 && ind < minInd){
+                minInd=ind;
+                type=keywords[i];
+            }
+        }
+        if(type==""){
+            result+=c;
+            break;
+        }
+        var selected = content.substring(p+type.length,minInd+1);
+        result+=selected;
+        if(type!='}'){
+            var id = idMap.get(type);
+            stack.push(id);
+            result+=open.get(id);
+        }else{
+            var id = stack.pop();
+            result+=close.get(id);
+        }
     }
     return result;
 }
