d44eaa0899d99af9183670dc52b9a63eb69bf2fd
RoelYoon <roelyoon2@gmail.com>
2024-06-21 15:31:25 +0900

    Update

--- public/index.js
+++ public/index.js
@@ -6,3 +6,16 @@ function uploadArticle(){
     a.href = "https://www.geeksforgeeks.org"; 
     document.body.appendChild(a); 
 }
+function httpGetAsync(theUrl, callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+
+}
+httpGetAsync("http://35.203.145.230:8099/articles",(res)=>{
+    console.log(res);
+});
