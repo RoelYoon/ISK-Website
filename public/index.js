0a2da018d1f48ddf93b613b3a8a93ce18c17b67c
RoelYoon <roelyoon2@gmail.com>
2024-06-26 23:05:47 +0900

    Update

--- public/index.js
+++ public/index.js
@@ -16,17 +16,34 @@ function httpGetAsync(theUrl, callback){
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 }
+let monthMap = {
+    "jan" : 1,
+    "feb" : 2,
+    "mar" : 3,
+    "apr" : 4,
+    "may" : 5,
+    "jun" : 6,
+    "jul" : 7,
+    "aug" : 8,
+    "sep" : 9,
+    "oct" : 10,
+    "nov" : 11,
+    "dec" : 12
+};
 httpGetAsync(`${address}article`,(res)=>{
     var js = JSON.parse(res);
     js.sort(function(a,b){
-        var yearA = parseInt(a.date.substring(0,4));
-        var yearB = parseInt(b.date.substring(0,4));
+        var monthA = monthMap[a.date.substring(0,3)];
+        var dayA = parseInt(a.date.match(/ (.*),/)[1]);
+        var yearA = parseInt(a.date.substring(a.date.length-4));
+        var yearB = parseInt(b.date.substring(b.date.length-4));
+        console.log(`${monthA}/${dayA}/${yearA}`)
         if(yearA==yearB){
-            var monthA = parseInt(a.date.substring(5,7));
-            var monthB = parseInt(b.date.substring(5,7));
+            var monthA = monthMap[a.date.substring(0,3)];
+            var monthB = monthMap[b.date.substring(0,3)];
             if(monthA==monthB){
-                var dayA = parseInt(a.date.substring(8,10));
-                var dayB = parseInt(b.date.substring(8,10));
+                var dayA = parseInt(a.date.match(/ (.*),/)[1]);
+                var dayB = parseInt(b.date.match(/ (.*),/)[1]);
                 return dayA > dayB ? -1 : 1;
             }else{
                 return monthA > monthB ? -1 : 1;
