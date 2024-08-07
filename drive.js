fa333a78b95af396f1eead5524e743502f0f2ad6
Roel Yoon <roelyoon2@gmail.com>
2024-08-07 16:20:14 +0900

    Update

--- drive.js
+++ drive.js
@@ -132,7 +132,7 @@ function extract(res){
                 (emptyStr? `` : imgCaptionState ? `<p class="imgCaption">` : `<p>`) +
                 (style.bold?`<strong>`:``) +
                 (style.italic?`<em>`:``) +
-                (emptyStr?``:elements[j].textRun.content.replace("\n",`<br>`)) +
+                elements[j].textRun.content.replace("\n",`<br>`) +
                 (style.italic?`</em>`:``) +
                 (style.bold?`</strong>`:``) +
                 (emptyStr? `` : `</p>`) +
